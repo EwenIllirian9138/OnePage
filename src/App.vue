39cd499af3535e076160073f85d01191947ad147
Ewen Billot <ewen.billot@Digital-Grenoble.local>
2018-03-05 13:37:48 +0100

    correction applique, ESLint pas config

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,72 @@
 <template>
   <div id="app">
-    <!--<img src="./assets/logo.png">-->
-    <!--<router-view/>-->
-    <toggle-button @change="onChangeEventHandler"/>
-
-    <toggle-button v-model="myDataVariable"/>
-
-    <toggle-button :value="false"
-                   color="#82C7EB"
-                   :sync="true"
-                   :labels="true"/>
-
-    <toggle-button :value="true"
-                   :labels="{checked: 'Foo', unchecked: 'Bar'}"/>
+    <img src="">
+    <h1>{{ message }}</h1>
+    <div>
+      <h4>
+        hide on machine off
+        <toggle-button :value="hideOffOwMachines" @change="hideOffOwMachines = !hideOffOwMachines"/>
+      </h4>
+    </div>
+    <table>
+        <OwMachine v-for="owMachine in owMachines"
+                 :key="owMachine.id"
+                 v-bind:OwMachine="owMachine"
+                 v-if="!hideOffOwMachines || owMachine.isOn">
+        </OwMachine>
+    </table>
+    <form @submit.prevent="addOwMachine" class="form">
+      <input type="text" placeholder="Nom de la machine" v-model="buffer.name">
+      <select name="status" v-model="buffer.isOn">
+        <option v-bind:value="true">ON</option>
+        <option v-bind:value="false">OFF</option>
+      </select>
+      <input type="submit" value="Ajouter">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app',
+  data () {
+    return {
+      hideOffOwMachines: false,
+      message: 'list of machines',
+      buffer: {
+        id: 3, name: 'Next machine name', isOn: true
+      },
+      owMachines: [
+        {id: 1, name: 'Machine Hanamura', isOn: true},
+        {id: 2, name: 'Machine Anubis', isOn: true},
+        {id: 3, name: 'Machine Volskaya', isOn: true},
+        {id: 4, name: 'Machine Horizon', isOn: true}
+      ]
+    }
+  },
+  methods: {
+    addOwMachine: function (e) {
+      this.buffer.id += 1
+      this.owMachines.push({
+        id: this.buffer.id,
+        name: this.buffer.name,
+        isOn: this.buffer.isOn
+      });
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+table {
+  display:  block;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
 import ToggleButton from 'vue-js-toggle-button'
 Vue.use(ToggleButton)
 
-Vue.config.productionTip = false
+Vue.component('OwMachine', {
+  template: '<tr :class="{ active: OwMachine.isOn }">{{OwMachine.name}}<toggle-button :value="OwMachine.isOn" @change="OwMachine.isOn = !OwMachine.isOn"/></tr>',
+  props: ['OwMachine']
+})
 
-/* eslint-disable no-new */
-new Vue({
+new Vue({ // eslint-disable-line no-new
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
